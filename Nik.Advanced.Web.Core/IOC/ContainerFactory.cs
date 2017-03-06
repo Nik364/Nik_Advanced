bfa43e05b6960dba68e8b1e685beadb0ad30cf93
NIK <Email@qq.com>
2017-03-07 01:02:52 +0800

    【EDIT】容器创建代码添加lock，确保线程安全

--- Nik.Advanced.Web.Core/IOC/ContainerFactory.cs
+++ Nik.Advanced.Web.Core/IOC/ContainerFactory.cs
@@ -12,35 +12,40 @@ namespace Nik.Advanced.Web.Core.IOC
     /// </summary>
     public class ContainerFactory
     {
+        static object syncHelper = new object();
         static Dictionary<string, IUnityContainer> unityContainerCache = new Dictionary<string, IUnityContainer>();
 
-        public static IUnityContainer CreateContainer(string name= "DefaultContainer")
+        public static IUnityContainer CreateContainer(string name = "DefaultContainer")
         {
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"CfgFiles\Unity.Config.xml");
             return CreateContainer(path, name);
         }
 
-
         public static IUnityContainer CreateContainer(string path, string name)
         {
-            if (unityContainerCache.ContainsKey(name))
+            if (!unityContainerCache.ContainsKey(name))
             {
-                return unityContainerCache[name];
+                //考虑到Web多线程模型下线程安全，此处应加锁
+                lock (syncHelper)
+                {
+                    if (!unityContainerCache.ContainsKey(name))
+                    {
+                        //获取config
+                        ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+                        fileMap.ExeConfigFilename = path;
+                        Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+                        UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
+
+                        //配置Unity
+                        IUnityContainer container = new UnityContainer();
+                        section.Configure(container, name);
+
+                        //缓存Container
+                        unityContainerCache.Add(name, container);
+                    }
+                }
             }
-
-            //获取config
-            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
-            fileMap.ExeConfigFilename = path;
-            Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
-            UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
-
-            //配置Unity
-            IUnityContainer container = new UnityContainer();
-            section.Configure(container, name);
-
-            //缓存Container
-            unityContainerCache.Add(name, container);
-            return container;
+            return unityContainerCache[name];
         }
     }
 }
