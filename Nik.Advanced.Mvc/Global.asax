fa942db8467d55b376a94420ab9aa798e122ccce
NIK <Email@qq.com>
2017-03-06 19:13:21 +0800

    【ADD】创建MVC项目

---
+++ Nik.Advanced.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nik.Advanced.Mvc.MvcApplication" Language="C#" %>
---
+++ Nik.Advanced.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nik.Advanced.Mvc.Startup))]
+namespace Nik.Advanced.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
